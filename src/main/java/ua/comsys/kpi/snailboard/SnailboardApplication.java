991040d5ee894e118e93ea121a5330805baa48c4
nas156 <31134655+nas156@users.noreply.github.com>
2021-03-06 14:26:16 +0200

    initial commit

---
+++ src/main/java/ua/comsys/kpi/snailboard/SnailboardApplication.java
@@ -0,0 +1,13 @@
+package ua.comsys.kpi.snailboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnailboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/comsys/kpi/snailboard/SnailboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.comsys.kpi.snailboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
