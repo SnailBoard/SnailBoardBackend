f6c0478662b726782bd5b87d9e37e01fafc77697
nas156 <31134655+nas156@users.noreply.github.com>
2021-03-06 15:28:14 +0200

    add health-check route

---
+++ src/main/java/ua/comsys/kpi/snailboard/test/TestController.java
@@ -0,0 +1,16 @@
+package ua.comsys.kpi.snailboard.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/health-check")
+public class TestController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "OK";
+    }
+
+}
