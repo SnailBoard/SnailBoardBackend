9f4462b762a307f54ec929afc4557fd67c8cf759
Pavlo Ivashchenko <pawloiwanov@gmail.com>
2021-05-22 20:17:15 +0300

    38: add test

--- src/test/java/ua/comsys/kpi/snailboard/board/facade/impl/BoardFacadeImplTest.java
+++ src/test/java/ua/comsys/kpi/snailboard/board/facade/impl/BoardFacadeImplTest.java
@@ -8,21 +8,29 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import ua.comsys.kpi.snailboard.board.dto.GetBoardResponse;
+import ua.comsys.kpi.snailboard.board.model.Board;
 import ua.comsys.kpi.snailboard.board.service.BoardService;
 import ua.comsys.kpi.snailboard.team.exception.InvalidTeamIdFormat;
 import ua.comsys.kpi.snailboard.team.model.Team;
 import ua.comsys.kpi.snailboard.team.service.TeamService;
+import ua.comsys.kpi.snailboard.user.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class BoardFacadeImplTest {
 
-    private static final String NAME = "NAME";
-    private static final String DESC = "DESC";
+    private static final String NAME_1 = "NAME_1";
+    private static final String DESC_1 = "DESC_1";
+    private static final String NAME_2 = "NAME_2";
+    private static final String DESC_2 = "DESC_2";
     private static final UUID UUID_1 = UUID.fromString("115655a0-0bbc-44fd-92b2-df2643fa147f");
     private static final String UUID_1_STRING = "115655a0-0bbc-44fd-92b2-df2643fa147f";
     private static final String INVALID_UUID_STRING = "invalid";
@@ -43,7 +51,7 @@ class BoardFacadeImplTest {
 
     @Test
     void shouldSaveBoardIfUserBelongsToTeam() {
-        Assertions.assertThrows(InvalidTeamIdFormat.class, () -> testingInstance.createInitial(NAME, DESC, INVALID_UUID_STRING));
+        Assertions.assertThrows(InvalidTeamIdFormat.class, () -> testingInstance.createInitial(NAME_1, DESC_1, INVALID_UUID_STRING));
     }
 
     @Test
@@ -51,8 +59,29 @@ class BoardFacadeImplTest {
         Team team = Team.builder().id(UUID_1).build();
         when(teamService.getTeamById(UUID_1)).thenReturn(team);
 
-        testingInstance.createInitial(NAME, DESC, UUID_1_STRING);
+        testingInstance.createInitial(NAME_1, DESC_1, UUID_1_STRING);
 
-        verify(boardService).createInitial(NAME, DESC, team);
+        verify(boardService).createInitial(NAME_1, DESC_1, team);
+    }
+
+    @Test
+    void shouldConvertTeamsToDto() {
+        List<User> users = spy(new ArrayList<>());
+        Team team = Team.builder().id(UUID_1).users(users).build();
+        List<Board> boards = new ArrayList<>();
+        Board board1 = Board.builder().name(NAME_1).description(DESC_1).build();
+        Board board2 = Board.builder().name(NAME_2).description(DESC_2).build();
+        boards.add(board1);
+        boards.add(board2);
+        when(teamService.getTeamById(UUID_1)).thenReturn(team);
+        when(boardService.getBoardsByTeam(team)).thenReturn(boards);
+        when(users.size()).thenReturn(3);
+
+        GetBoardResponse result = testingInstance.getBoardsByTeam(UUID_1_STRING);
+
+        assertThat(result.getBoards().size(), is(2));
+        assertThat(result.getBoards().get(0).getName(), is(NAME_1));
+        assertThat(result.getBoards().get(1).getDescription(), is(DESC_2));
+        assertThat(result.getMemberCount(), is(3));
     }
 }
